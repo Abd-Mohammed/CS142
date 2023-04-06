3b55c6a2c06c1d854ca5cc3a01d4906f17224049
Abd Al-Rahman Odeh <comodeh@gmail.com>
2023-04-07 01:12:52 +0300

    Update run-tests-using-node.js

--- Project 2/run-tests-using-node.js
+++ Project 2/run-tests-using-node.js
@@ -1,37 +1,17 @@
 'use strict';
-/*
- * run-test-using-node.js - Run the cs142 project #2 test code using Node.js rather than in the
- * browser.  The browser has a better debugging environment so this is more a demonstration that the
- * JavaScript works in both places. To run the test:
- *   node run-tests-using-node.js
- */
 
-/* jshint node: true */
-
-// We need the Node.js file system access module (fs) and the Node.js JavaScript virtual machine
-// access.
 var fs = require('fs');
 var vm = require('vm');
 
-// Build an emulation of the browser's script tag processing where everything is in a global
-// space and goes under the name window.
 
-global.window = global;  // The browser script assume global is available under the name window
 
-/**
- * processScriptFromFile - Emulate the effects of a script tag in the browser by running the
- * contents of the file as a script with its scope being the global object.
- * @param {string} filename - File name of script to load and run.
- */
+global.window = global; 
+
 function processScriptFromFile(filename) {
-    // Warning: Ugly, un-Node.js-like code warning:
-    // In order to emulate the browser JavaScript environment we need to undo the default isolation
-    // in node modules. By directly calling into the Node.js vm.Script() API we can have
-    // all the JavaScripts files run in the same context like on the browser.
+  
     try {
         new vm.Script(fs.readFileSync(filename).toString(), {filename: filename}).runInThisContext();
     } catch (err) {
-        // fs.readFileSync communicates errors using exceptions. We log but otherwise ignore errors
         console.error('Error processing', filename, ':', err.message);
     }
 }
@@ -63,4 +43,4 @@ var arrayDiff = function(a,b) {
     return b.filter(function(i) {return a.indexOf(i) < 0;}).concat(a.filter(function(i) {return b.indexOf(i) < 0;}));
 };
 
-process.exit(Number(!testWorked));  // For npm: Return process status code 0 on success, 1 on failure.
+process.exit(Number(!testWorked));
