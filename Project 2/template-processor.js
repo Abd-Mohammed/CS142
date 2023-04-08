9609856de35973b2e559d4e8b443558248e42184
Abd Al-Rahman Odeh <comodeh@gmail.com>
2023-04-08 04:01:53 +0300

    Update template-processor.js

--- Project 2/template-processor.js
+++ Project 2/template-processor.js
@@ -1,18 +1,19 @@
 'use strict';
 
-function Cs142TemplateProcessor(template){
-    this.template = template;    
+function TemplateProcessor(template) {
+  this.template = template;
 }
 
-Cs142TemplateProcessor.prototype.fillIn = function(dictionary){
-    var res = this.template;
+TemplateProcessor.prototype.fillIn = function(dictionary) {
+  var res = this.template;
 
-    for(var key in dictionary){
-        var pattern = new RegExp('\{\{' + key + '\}\}','g');
-        res = res.replace(pattern, dictionary[key]);  	
-    }
-        
-    res = res.replace(/{{[^{}]*}}/g,'');       
-       
-    return res;
+  // eslint-disable-next-line guard-for-in
+  for (var key in dictionary) {
+    var pattern = new RegExp('{{' + key + '}}', 'g');
+    res = res.replace(pattern, dictionary[key]);
+  }
+
+  res = res.replace(/{{[^{}]*}}/g, '');
+
+  return res;
 };
